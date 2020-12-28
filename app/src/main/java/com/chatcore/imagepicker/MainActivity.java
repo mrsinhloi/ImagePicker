079c7bbce1c22cedc67475224dfdbc0ead0ef770
PhamPhuong <vsoftphuong@gmail.com>
2020-12-28 14:42:32 +0700

    #1.0.6: - Update gradle, library - Fix report

--- app/src/main/java/com/chatcore/imagepicker/MainActivity.java
+++ app/src/main/java/com/chatcore/imagepicker/MainActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.nguyenhoanglam.imagepicker.activity.ImagePicker;
+import com.nguyenhoanglam.imagepicker.activity.ImagePickerActivity;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImagePicker.create(this)
+                .single() // single mode
+                .imageTitle(getText(R.string.app_name).toString())
+                //.limit(1) // max images can be selected
+                .showCamera(true) // show camera or not (true by default)
+                .start(1);
     }
 }
